0c9d9e5f23ba8cd083a8a0deca8b442f3e57d66d
tahbristol <tahbristol@gmail.com>
2017-08-03 00:02:21 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,11 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    if song.artist
+
+      "<a href='/artists/#{song.artist.id}'>#{song.artist.name}</a>".html_safe
+    else
+        "<a href='/songs/#{song.id}/edit'>Add Artist</a>".html_safe
+    end
+  end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,5 @@
 module SongsHelper
-
+  def link_to_edit(text, href)
+    "<a href='#{href}'>#{text}</a>".html_safe
+  end
 end
