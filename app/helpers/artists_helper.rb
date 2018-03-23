883a0b7c7a808a0783e985c1718d1422aaa04d6c
petitlapin86 <skinnerpa86@yahoo.com>
2018-03-23 14:01:02 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
-end
+
+  def display_artist(song) #should respond to display_artist with one argument
+     if song.artist
+       link_to song.artist.name, song.artist #displays a link to the artist page 
+     else
+       link_to 'Add Artist', edit_song_path(song) #displays a link to the edit song
+     end
+   end
+ end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
-  end
+    def artist_name
+      artist.name if artist #gets the artists name
+    end
 
-  def artist_name=(name)
+    def artist_name=(name)
+      a = Artist.find_or_create_by(name: name) #finds artist if it already exists
+      self.artist = a #can set the artist via name
+    end
   end
-end
