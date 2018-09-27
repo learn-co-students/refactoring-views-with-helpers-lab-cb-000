37cddd787e917eed3f0211d3148d2f2072cf0eb3
dapawn <dapawn@gmail.com>
2018-09-27 14:49:37 -0400

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+
+  def display_artist(song)
+    link_to_if(song.artist.nil?, "Add Artist", edit_song_path(song) ) do
+      link_to song.artist.name, song.artist
+    end
+  end
+
 end
