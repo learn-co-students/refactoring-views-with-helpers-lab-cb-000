6a4c5b8f8eec45484e90910c4d57dff8876332d3
Chris Franklin <cf73021@gmail.com>
2018-12-18 21:02:51 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
-
+  def display_artist(song)
+    if song.artist.nil?
+      link_text = 'Add Artist'
+      path = edit_song_path(song)
+    else
+      link_text = song.artist_name
+      path = artist_path(song.artist)
+    end
+    render inline: "<%= link_to link_text, path %>", locals: { link_text: link_text, path: path }
+  end
 end
